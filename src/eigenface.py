12514b3db33800f287ac118ee5483f5537aaa195
althaafka <althaafka004@gmail.com>
2022-11-15 07:54:58 +0700

    update eigenface

--- src/eigenface.py
+++ src/eigenface.py
@@ -9,25 +9,41 @@ print(mat)
 def mean(training_image):
     return np.mean(training_image, axis=0)
 
-average = mean(mat)
-print(average)
+# average = mean(mat)
+# print(average)
 
 # selisih training image dengan mean
 def Selisih(average,training_image):
     selisih = []
     for i in range(len(training_image)):
-        selisih.append(np.absolute(np.subtract(training_image[i],average)))
+        # selisih.append(np.absolute(np.subtract(training_image[i],average)))
+        selisih.append(np.subtract(training_image[i],average))
+    row = len(selisih[0])*len(selisih[0][0])
+    col = len(selisih)
+    selisih = np.reshape(selisih,(col,row))
+    selisih = np.transpose(selisih)
     return selisih
 
-selisih = Selisih(average,mat)
-print(selisih)
+# selisih = Selisih(average,mat)
+# print(selisih)
 
 # matrix covarian
 def Covarian(selisih):
-    Aa = np.concatenate(selisih,axis=1)
-    At = np.transpose(Aa)
-    return np.matmul(Aa,At)
+    Aa = selisih
+    At = np.transpose(selisih)
+    # return np.matmul(At,Aa)
+    return np.divide(np.matmul(At,Aa),len(selisih))
 
-covarian = Covarian(selisih)
-print(covarian)
+# covarian = Covarian(selisih)
+# print(covarian)
 
+def Eigenfaces(imgArr):
+    average = mean(imgArr)
+    selisih = Selisih(average,imgArr)
+    covarian = Covarian(selisih)
+    covarian = np.int_(covarian)
+    print(covarian)
+    (eigenvalue,eigenvector) = np.linalg.eig(covarian)
+    eigenface = np.matmul(eigenvector,np.transpose(selisih))
+    eigenface = np.reshape(eigenface,(-1,256,256))
+    return eigenface
--- src/main_test.py
+++ src/main_test.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 from os import listdir
 import extractor
+import eigenface
 
 
 datasetPath = os.getcwd() + "\\test\\dataset"
@@ -10,20 +11,23 @@ inputPath = os.getcwd() + "\\test\\input"
 # print(datasetPath)
 
 # Extract all dataset to image_train
-image_arr = extractor.extractImages(datasetPath)
-print(image_arr)
+images_arr = extractor.extractImages(datasetPath)
+print(images_arr)
 
 # Extract input image
 test_face = extractor.extractImages(inputPath)[0]
 
 # Checking the size of a single image
-print(test_face.shape)
+# print(test_face.shape)
 
 # Diplay image 1
-cv2.namedWindow('image', cv2.WINDOW_AUTOSIZE)
-cv2.imshow('image', test_face)
-cv2.waitKey()
+# cv2.namedWindow('image', cv2.WINDOW_AUTOSIZE)
+# cv2.imshow('image', test_face)
+# cv2.waitKey()
 
 
-
+eigenface = eigenface.Eigenfaces(images_arr)
+for i in range(len(eigenface)):
+    string = "image"+str(i)+".jpg"
+    cv2.imwrite(string,np.int_(eigenface[i]))
 
